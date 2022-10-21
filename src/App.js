ca72febdf7d9edecb637e6d5b49fc54701049abd
DESKTOP-6VLJSIH\unmes <undermess@gmail.com>
2022-10-21 13:16:15 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Routes>
+                <Route path={'/'} element={<Layout/>}>
+                    <Route path={'/'} element={<HomePage/>}>
+                        <Route path={':genreName'} element={<MoviesOfGenre/>}/>
+                        <Route path={':movieId'} element={<MovieInfoPage/>}/>
+                    </Route>
+                    <Route path={'movies'} element={<MoviesPage/>}>
+                        <Route path={':movieId'} element={<MovieInfoPage/>}/>
+                    </Route>
+                    <Route path={'genres'} element={<GenresPage/>}>
+                        <Route path={':genreName'} element={<MoviesOfGenre/>}>
+                            <Route path={':movieId'} element={<MovieInfoPage/>}/>
+                        </Route>
+                    </Route>
+                    <Route path={'*'} element={<NotFoundPage/>}/>
+                </Route>
+            </Routes>
+        </div>
+    );
 }
 
-export default App;
+export {App};
